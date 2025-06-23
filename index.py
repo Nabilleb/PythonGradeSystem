21a5d06de3bc1bfe846087c03cdcf7fa5f32b831
Nabilleb <nabilgreen500@gmail.com>
2025-06-23 16:13:20 +0300

    created a function to add records

--- index.py
+++ index.py
@@ -1,8 +1,17 @@
-class_journal = {
-    'Layla': [89, 91, 86],
-    'Tariq': [77, 84, 73],
-    'Jana': [100, 97, 94],
-    'Ziad': [62, 71, 75]
-    }
+def create_class_journal(records):
+    class_journal = {}
+    for name, grade in records:
+        if name not in class_journal:
+            class_journal[name] = []
+        class_journal[name].append(grade)
+    return class_journal
 
-print(class_journal)
+records = [
+    ["Layla", 89], ["Tariq", 77], ["Layla", 91], ["Jana", 100], ["Tariq", 84],
+    ["Ziad", 62], ["Jana", 97], ["Tariq", 73], ["Ziad", 71], ["Layla", 86],
+    ["Jana", 94], ["Ziad", 75]
+]
+
+journal = create_class_journal(records)
+print(journal['Layla'])
+
